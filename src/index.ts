b4224f0de6944c9e7305ad8b008326be46811c0c
Rwan <rwan.souza@hotmail.com>
2021-03-22 16:20:54 -0300

    Started

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import routes from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => console.log("Server started at http://localhost:3333"));
