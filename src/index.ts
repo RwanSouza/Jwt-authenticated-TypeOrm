f9b7ca971e06ab2d0f2ece78e97a94a3100c13ed
Rwan <rwan.souza@hotmail.com>
2021-03-22 16:51:39 -0300

    Configuration TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express from "express";
+import 'reflect-metadata';
+import express from 'express';
 
-import routes from "./routes";
+import './database/connect';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.listen(3333, () => console.log("Server started at http://localhost:3333"));
+app.listen(3333, () => console.log('Server started at http://localhost:3333'));
